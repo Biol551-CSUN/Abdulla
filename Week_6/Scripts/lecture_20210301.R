b2c936240af8001c0a22e88149c794d409561848
jasminabdulla <jasmineaabdulla@gmail.com>
2021-03-03 11:45:14 -0800

    submitting week 6

---
+++ Week_6/Scripts/lecture_20210301.R
@@ -0,0 +1,50 @@
+### Lecture 2021-03-01 ####
+### Week 6 ###
+
+
+install.packages("rmarkdown")
+install.packages('tinytex') #this will be helpful if you want a PDF doc
+
+FOR MARKDOWN:
+  I **love** RMarkdown #bold
+I *love* Markdown #italics
+
+To add a new line: hit the space bar twice after a line if you want the following text on a newline
+
+#HEADER 1. the more hashtags, the smaller it is 
+
+Lists: 1. 1. 1. -> Item 1, 2, 3 (itll numerize it correctly)
+
+For images: ![CSUN logo] (link here)
+without the ! in the beginning, itll create a text link instead 
+
+```{r} #shows you want to code
+(LINK FOR R MARKDOWN REFERENCE GUIDE IN LECTURE SLIDES)
+
+install.packages("beepr")
+
+library(beepr)
+
+beep(5)
+beep(9)
+beep(3)
+beep(10)
+beep(20)
+beep(2)
+beep(4)
+beep(6)
+beep(7)
+beep(8)
+beep(11)
+beep(9)
+beep(12)
+beep(13)
+beep(14)
+beep(15)
+beep(16)
+beep(17)
+beep(18)
+beep(19)
+beep(5)
+beep(3)
+beep(8)
